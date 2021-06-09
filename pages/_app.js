9705befa7e0ae2b7eadbcf5238bbdf8805de4971
kmocorro <kevinmocorro.github@gmail.com>
2021-06-09 09:36:18 +0800

    connecting to github

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import "antd/dist/antd.css"
+// import "antd/dist/antd.dark.css"
+
+import React from 'react'
+
+export default function App({ Component, pageProps }){
+  return (
+    <Component {...pageProps} />
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>I am not connected to GitHub</p>
+    </div>
+  )
+}
+
+export default Index
